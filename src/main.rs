6e64d20d926f04d4b82bc294f3fa79c741ef3b2b
Calleum Pecqueux <calleum.pecqueux@gmail.com>
2021-06-19 12:44:09 +1000

    Upd: use postgres as task datastore

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
-use chrono::prelude::*;
-use clap::Clap;
-
-use std::collections::HashMap;
-use std::fmt;
-use std::io::prelude::*;
+/*use serde_json::Result;*/
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
+use chrono::prelude::*;
+use clap::Clap;
+use postgres::{Client, Error, NoTls};
+
 #[derive(Debug, Clap)]
 #[clap(
-    name = "todo-rs",
-    about = "a simple todo app to set timers and switch tasks"
+name = "todo-rs",
+about = "a simple todo app to set timers and switch tasks"
 )]
 struct Opt {
     #[clap(subcommand)]
@@ -64,43 +61,64 @@ enum Args {
 }
 
 #[derive(Hash, Eq, PartialEq)]
-#[derive(Debug, Serialize, Deserialize)]
+ #[derive(Debug)]
 struct Task {
+    _id: i32,
     name: String,
     description: String,
     duration: i32,
     due_date: String,
+    start_date: String,
 }
 
 impl Task {
-    fn new(name: String, description: String, duration: i32) -> Result<Task> {
-
+    fn new(client: &Client, name: String, description: String, duration: i32) -> Result<Task, Error> {
         let task = Task {
-            name,
-            description,
+            _id: 0,
+            name: name.to_string(),
+            description: description.to_string(),
             duration,
+            start_date: Local::today().to_string(),
             due_date: Local::today().to_string(),
         };
-                Ok(task)
+        client.execute(
+            "INSERT INTO task (name, description, duration, start_date, due_date) values ($1, $2, $3, $4, $5)",
+            &[&task.name, &task.description, &task.duration, &task.start_date, &task.due_date],
+        )?;
+
+        Ok(task)
     }
 
-    fn read<P: AsRef<Path>>(path: P) -> Result<Task> {
+    /* fn read<P: AsRef<Path>>(path: P) -> Result<Task, Error> {
 
-        let file = File::open(path).unwrap();
-        let reader = BufReader::new(file);
+         let file = File::open(path).unwrap();
+         let reader = BufReader::new(file);
 
-        let t = serde_json::from_reader(reader)?;
+         let t = serde_json::from_reader(reader)?;
 
-        Ok(t)
+         Ok(t)
 
-    }
+     }*/
 }
 
-fn main() -> Result<()>{
- //   let mut tasks = HashMap::new();
+fn main() -> Result<(), Error> {
+
+    //   let mut tasks = HashMap::new();
+    let mut client = Client::connect("postgresql://postgres:postgres@localhost/library", NoTls)?;
+
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS task (
+            id      SERIAL PRIMARY KEY,
+            name  VARCHAR NOT NULL,
+            duration   INTEGER NOT NULL,
+            description  VARCHAR NOT NULL,
+            due_date  VARCHAR NOT NULL,
+            start_date  VARCHAR NOT NULL,
+        )
+        ");
     let opts = Opt::parse();
 
-    let mut tasks= Vec::new();
+    let mut tasks: Vec<Task> = Vec::new();
 
 
     println!("{:?}", opts);
@@ -111,13 +129,23 @@ fn main() -> Result<()>{
             description,
         } => {
             println!("add was used");
-            let new_task = Task::new(name, description, duration).unwrap();
+            let new_task = Task::new(&client, name, description, duration).unwrap();
 
-
-
-            let b = std::path::Path::new("db.json").exists();
+            for row in client.query("SELECT id, name, duration, description, due_date, start_date\
+             FROM task", &[])? {
+                let task = Task {
+                    _id: row.get(0),
+                    name: row.get(1),
+                    duration: row.get(2),
+                    description: row.get(3),
+                    due_date: row.get(4),
+                    start_date: row.get(5),
+                };
+                println!("task {} is {}", task.name, task.description);
+            }
+            /*            let b = std::path::Path::new("db.json").exists();
             if b {
-                let f = File::open("db.json").expect("file errpr");
+                let f = File::open("db.json").expect("file error");
                 let buf = BufReader::new(f);
                 let mut c: Vec<Task> = serde_json::from_reader(buf).expect("error reading json into a task type");
                 tasks.append(&mut c);
@@ -129,17 +157,15 @@ fn main() -> Result<()>{
             std::fs::write("db.json", content).unwrap();
 
             println!("{:?}", tasks)
+            */
         },
         Args::Status { name, all } => {
             println!("{:?}", tasks);
-
-        },
+        }
         Args::Remove { name, all } => {
             println!("remove was used");
         }
         _ => eprintln!("footsies")
     }
-
-
     Ok(())
 }
